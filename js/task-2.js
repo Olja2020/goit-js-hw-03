f468ca3e4ad964f42080f4142622fdf48c3ccea1
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-01-16 10:06:38 +0200

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+"use strict";
+function formatMessage(message, maxLength) {
+  let messageLength = message.length;
+
+  if (messageLength > maxLength) {
+    const newMessage = message.slice(0, maxLength);
+    const messageAddDot = newMessage + "...";
+    return messageAddDot;
+  } else {
+    return message;
+  }
+}
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
